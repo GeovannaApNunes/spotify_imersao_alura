0e356663b1d4327de81626c5645e93860e6bc3fc
Geovanna Nunes <geovannanunes349@gmail.com>
2025-02-03 12:14:08 -0300

    Add files via upload

---
+++ Spotify_clone/script.js
@@ -0,0 +1,32 @@
+//BOM DIA | BOA TARDE | BOA NOITE
+
+
+const greetingElement = document.getElementById("greeting");
+
+// Obtém a hora atual do sistema
+const currentHour = new Date().getHours();
+
+// Forma mais simples
+const greetingMessage =
+  currentHour >= 5 && currentHour < 12
+    ? "Bom dia"
+    : currentHour >= 12 && currentHour < 18
+    ? "Boa tarde"
+    : "Boa noite";
+
+greetingElement.textContent = greetingMessage;
+
+
+const container = document.querySelector(".offer__list-item");
+
+const observer = new ResizeObserver(() => {  
+  const containerWidth = container.offsetWidth; 
+  const numColumns = Math.floor(containerWidth / 200); 
+ 
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
+
+  console.log({ container });
+  console.log({ numColumns });
+});
+
+observer.observe(container);
